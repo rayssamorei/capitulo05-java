93dbad4518a93cb3d8809036a5a281aed621fa0e
rayssamorei <rayssa32alvess@gmail.com>
2023-10-10 09:44:07 -0300

    Questão 10

---
+++ Atividade10.java
@@ -0,0 +1,39 @@
+package atividades;
+
+import java.util.Scanner;
+
+/**
+ * Faça um programa que receba dez números, calcule e mostre a soma dos números
+ * pares e a soma dos números primos.
+ */
+
+public class Atividade10 {
+    public static void main(String[] args) {
+        var entrada = new Scanner(System.in);
+        int num, somaPar = 0, somaPrimo = 0, primo = 0;
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Informe o " + (i+1) +"º número: ");
+            num = entrada.nextInt();
+            if (num % 2 == 0){
+                somaPar += num;
+            }
+
+            for (int j = 2; j <= num; j++) {
+                if (num % j == 0) {
+                    primo++;
+                }
+            }
+            if(primo == 1){
+                somaPrimo += num;
+            }
+            primo = 0;
+        }
+
+        System.out.println("Soma pares: "+ somaPar);
+        System.out.println("Soma primos: "+ somaPrimo);
+
+        entrada.close();
+    
+    }
+}
